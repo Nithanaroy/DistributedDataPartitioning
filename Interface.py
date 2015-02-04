c40c1322bb4e795d746fc0e1c19e803355d367b1
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-02-04 01:22:35 -0700

    Interface file improved significantly - added hooks

--- Interface.py
+++ Interface.py
@@ -5,14 +5,14 @@
 
 import psycopg2
 
-RATINGS_TABLE_NAME = 'Ratings'
+DATABASE_NAME = 'dds_assgn1'
 
 
 def getopenconnection(user='postgres', password='1234', dbname='dds_assgn1'):
     return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
 
 
-def loadratings(ratingsfilepath, openconnection):
+def loadratings(ratingstablename, ratingsfilepath, openconnection):
     pass
 
 
@@ -56,12 +56,51 @@ def create_db(dbname):
     con.close()
 
 
+# Middleware
+def before_db_creation_middleware():
+    # Use it if you want to
+    pass
+
+
+def after_db_creation_middleware(databasename):
+    # Use it if you want to
+    pass
+
+
+def before_test_script_starts_middleware(openconnection, databasename):
+    # Use it if you want to
+    pass
+
+
+def after_test_script_ends_middleware(openconnection, databasename):
+    # Use it if you want to
+    pass
+
+
 if __name__ == '__main__':
     try:
-        create_db('dds_assgn1')
+
+        # Use this function to do any set up before creating the DB, if any
+        before_db_creation_middleware()
+
+        create_db(DATABASE_NAME)
+
+        # Use this function to do any set up after creating the DB, if any
+        after_db_creation_middleware(DATABASE_NAME)
 
         with getopenconnection() as con:
+            # Use this function to do any set up before I starting calling your functions to test, if you want to
+            before_test_script_starts_middleware(con, DATABASE_NAME)
+
+            # Here is where I will start calling your functions to test them. For example,
             loadratings('ratings.dat', con)
+            # ###################################################################################
+            # Anything in this area will not be executed as I will call your functions directly
+            # so please add whatever code you want to add in main, in the middleware functions provided "only"
+            # ###################################################################################
+
+            # Use this function to do any set up after I finish testing, if you want to
+            after_test_script_ends_middleware(con, DATABASE_NAME)
 
     except Exception as detail:
         print "OOPS! This is the error ==> ", detail
